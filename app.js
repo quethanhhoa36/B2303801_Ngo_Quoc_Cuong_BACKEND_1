b11205ee8d3a4bf5bbb01c55cc31dd546ea93e3c
quethanhhoa36 <cngo98279@gmail.com>
2024-09-19 21:47:06 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const ApiError = require("./app/api-error");
+const contactsRouter=require("./app/routes/contact.route");
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/contacts",contactsRouter);
+
+app.use((req,res,next) =>{
+    return next(new ApiError(404,"Resource not found"));
+});
+
+app.use((error,req,res,next)=>{
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to contact book aplication." });
 });
-
-module.exports=app;
+module.exports = app;
